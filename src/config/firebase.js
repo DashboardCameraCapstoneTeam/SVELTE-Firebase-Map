cced4ab1b15f6b21d2d52e40eabad79e9f86ba47
Alex Canales <alexcanales766@gmail.com>
2022-09-12 23:22:38 -0400

    Added login page

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -16,5 +16,4 @@ firebase.initializeApp(config);
 const db = firebase.firestore();
 const auth = firebase.auth();
 const googleProvider = new firebase.auth.GoogleAuthProvider();
-
 export { auth, db, googleProvider };
