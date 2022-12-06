b13866273b4dacdbde6318613350b52a5e1ff6ed
Jasmine <jasminja@sheridancollege.ca>
2022-12-05 21:59:31 -0500

    Street View Initialized

---
+++ src/components/menu/StreetView.svelte
@@ -0,0 +1,35 @@
+<script>
+    import {onMount} from 'svelte/internal';
+    import {onDestroy} from 'svelte';
+
+
+    export let selectedPOI;
+
+    let streetViewObject = null;
+    let streetViewContainer = null;
+    let errro = null;
+
+    const initializeStreetView = () => {
+        try{
+            streetViewObject = new google.maps.StreetViewPanorama(streetViewContainer,{
+                position: selectedPOI,
+                pov:{
+                    heading: 34,
+                    pitch:10
+                }
+            });
+        }catch (err) {
+            error = err;
+        }
+    };
+
+    onMount(() => {
+        try{
+            if(selectedPOI !==null){
+                initializeStreetView();
+            }
+        } catch(err){
+            error = err;
+        }
+    });
+</script>
